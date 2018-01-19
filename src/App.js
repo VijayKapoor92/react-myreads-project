f4d70a6daeacf1e3d1b79ace35f8eb4270dcfa75
VijayKapoor92 <vijayk_1@hotmail.com>
2018-01-19 20:35:15 -0200

    Troca tags a pela tag link

--- src/App.js
+++ src/App.js
@@ -34,14 +34,14 @@ class App extends React.Component {
                             </div>
                         </div>
                         <div className="open-search">
-                            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                            <Link to='/search' />
                         </div>
                     </div>
                 )} />
                 <Route path='/search' render={() => (
                     <div className="search-books">
                         <div className="search-books-bar">
-                            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                            <Link className="close-search" to="/" />
                             <div className="search-books-input-wrapper">
                                 <input type="text" placeholder="Search by title or author"/>
                             </div>
